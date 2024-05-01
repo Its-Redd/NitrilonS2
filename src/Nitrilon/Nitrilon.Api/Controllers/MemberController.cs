bcdadbfe624d36240d76df9b8a933e71fb030e4e
Simon Piihl Rasmussen <61151@edu.campusvejle.dk>
2024-05-01 13:41:02 +0200

    Create MemberController

---
+++ src/Nitrilon/Nitrilon.Api/Controllers/MemberController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Nitrilon.Api.Controllers
+{
+    public class MemberController : ControllerBase
+    {
+        [HttpGet]
+        [Route("api/member")]
+        public IActionResult GetAllMembers()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("api/member")]
+        public IActionResult CreateMember()
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("api/member")]
+        public IActionResult DeleteMember()
+        {
+            return Ok();
+        }
+        
+
+
+    }
+}
